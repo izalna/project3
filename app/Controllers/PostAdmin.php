58a9f9951dd415bd21d23e738a071bef021f2dae
izalna <rizalna1978@gmail.com>
2025-07-16 20:32:07 +0700

    add view admin_post_create

--- app/Controllers/PostAdmin.php
+++ app/Controllers/PostAdmin.php
@@ -3,94 +3,91 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\PostModel;
 use CodeIgniter\Exceptions\PageNotFoundException;
 
 class PostAdmin extends BaseController
 {
     public function index()
-    {
+	{
         $post = new PostModel();
         $data['posts'] = $post->findAll();
-
-        return view('admin/admin_post_list', $data);
+		echo view('admin/admin_post_list', $data);
     }
 
     //--------------------------------------------------------------
-
+    
     public function preview($id)
-    {
-        $post = new PostModel();
-        $data['post'] = $post->where('id', $id)->first();
-
-        if (!$data['post']) {
-            throw PageNotFoundException::forPageNotFound();
-        }
-
-        return view('post_detail', $data);
+	{
+		$post = new PostModel();
+		$data['post'] = $post->where('id', $id)->first();
+		
+		if(!$data['post']){
+			throw PageNotFoundException::forPageNotFound();
+		}
+		echo view('post_detail', $data);
     }
 
-    //--------------------------------------------------------------
-
+    //--------------------------------------------------------------------------
+    
     public function create()
     {
-        $validation = \Config\Services::validation();
-        $validation->setRules([
-            'title' => 'required'
-        ]);
-
+        // lakukan validasi
+        $validation =  \Config\Services::validation();
+        $validation->setRules(['title' => 'required']);
         $isDataValid = $validation->withRequest($this->request)->run();
 
-        if ($isDataValid) {
+        // jika data valid, simpan ke database
+        if($isDataValid){
             $post = new PostModel();
             $post->insert([
-                'title'   => $this->request->getPost('title'),
-                'content' => $this->request->getPost('content'),
-                'status'  => $this->request->getPost('status'),
-                'slug'    => url_title($this->request->getPost('title'), '-', true)
+                "title" => $this->request->getPost('title'),
+                "content" => $this->request->getPost('content'),
+                "status" => $this->request->getPost('status'),
+                "slug" => url_title($this->request->getPost('title'), '-', TRUE)
             ]);
-
-            return redirect()->to('admin/post');
+            return redirect('admin/post');
         }
-
-        return view('admin/admin_post_create');
+		
+        // tampilkan form create
+        echo view('admin/admin_post_create');
     }
 
-    //--------------------------------------------------------------
+    //--------------------------------------------------------------------------
 
     public function edit($id)
     {
+        // ambil artikel yang akan diedit
         $post = new PostModel();
         $data['post'] = $post->where('id', $id)->first();
-
-        $validation = \Config\Services::validation();
+        
+        // lakukan validasi data artikel
+        $validation =  \Config\Services::validation();
         $validation->setRules([
-            'id'    => 'required',
+            'id' => 'required',
             'title' => 'required'
         ]);
-
         $isDataValid = $validation->withRequest($this->request)->run();
-
-        if ($isDataValid) {
+        // jika data vlid, maka simpan ke database
+        if($isDataValid){
             $post->update($id, [
-                'title'   => $this->request->getPost('title'),
-                'content' => $this->request->getPost('content'),
-                'status'  => $this->request->getPost('status')
+                "title" => $this->request->getPost('title'),
+                "content" => $this->request->getPost('content'),
+                "status" => $this->request->getPost('status')
             ]);
-
-            return redirect()->to('admin/post');
+            return redirect('admin/post');
         }
 
-        return view('admin/admin_post_update', $data);
+        // tampilkan form edit
+        echo view('admin/admin_post_update', $data);
     }
 
-    //--------------------------------------------------------------
+    //--------------------------------------------------------------------------
 
-    public function delete($id)
-    {
+	public function delete($id){
         $post = new PostModel();
         $post->delete($id);
-
-        return redirect()->to('admin/post');
+        return redirect('admin/post');
     }
 }
