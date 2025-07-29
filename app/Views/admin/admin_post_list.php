7f9f289d8e7f07f79040e600833d688710cb1365
izalna <rizalna1978@gmail.com>
2025-07-29 23:46:05 +0700

    fixing date create data

--- app/Views/admin/admin_post_list.php
+++ app/Views/admin/admin_post_list.php
@@ -76,7 +76,7 @@
   <div class="container py-4">
     <footer class="pt-3 mt-4 text-muted border-top">
       <div class="container">
-        &copy; <?= date('Y') ?>
+        &copy; <?= date(format: 'Y') ?>
       </div>
     </footer>
   </div>
--- app/Views/admin/admin_post_update.php
+++ app/Views/admin/admin_post_update.php
@@ -72,7 +72,7 @@
     <div class="container py-4">
         <footer class="pt-3 mt-4 text-muted border-top">
             <div class="container">
-                &copy; <?= Date('Y') ?>
+                &copy; <?= Date(format: 'Y') ?>
             </div>
         </footer>
     </div>
