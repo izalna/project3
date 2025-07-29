bad4009d810c2ac94e4e769b9bee53b19a544818
izalna <rizalna1978@gmail.com>
2025-07-29 22:02:34 +0700

    change all page titles

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>MyBlog</title>
+	<title>AfifGadget.com</title>
 
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>" />
--- app/Views/post_detail.php
+++ app/Views/post_detail.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>MyBlog</title>
+	<title>AfifGadget.com</title>
 
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>" />
@@ -15,7 +15,7 @@
 
 	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
 		<div class="container">
-			<a class="navbar-brand" href="<?= base_url() ?>">MyBlog</a>
+			<a class="navbar-brand" href="<?= base_url() ?>">Afif Gadget ID</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
